99485d75a2bc2be611937d35423444709a385e27
Placida Rebello <placidarebello12@gmail.com>
2019-12-17 18:05:24 +0530

    Creating extension for startup class

---
+++ IssueTracker/ApiConfig/TransientExtension.cs
@@ -0,0 +1,29 @@
+﻿using BussinessLogic;
+using BussinessLogic.Interfaces;
+using DataAccess;
+using DataAccess.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssueTracker.ApiConfig
+{
+    public static class TransientExtension
+    {
+        public static void AddCustomTransientServices(this IServiceCollection services) 
+        {
+            services.AddTransient<IIssuesLogic, IssuesLogic>();
+            services.AddTransient<IIssuesEngine, IssuesEngine>();
+
+            services.AddTransient<IStatusLogic, StatusLogic>();
+            services.AddTransient<IStatusEngine, StatusEngine>();
+
+            services.AddTransient<IIssueTypeLogic, IssueTypeLogic>();
+            services.AddTransient<IIssueTypeEngine, IssueTypeEngine>();
+
+            services.AddTransient<IRegisterLogic, RegisterLogic>();
+        }
+    }
+}
--- IssueTracker/Startup.cs
+++ IssueTracker/Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using ServiceModel.Type;
+using IssueTracker.ApiConfig;
 
 namespace IssueTracker
 {
@@ -40,73 +41,19 @@ namespace IssueTracker
 
             services.AddAutoMapper(typeof(Startup));
 
-            services.AddTransient<IIssuesLogic, IssuesLogic>();
-            services.AddTransient<IIssuesEngine, IssuesEngine>();
-
-            services.AddTransient<IStatusLogic, StatusLogic>();
-            services.AddTransient<IStatusEngine, StatusEngine>();
-
-            services.AddTransient<IIssueTypeLogic, IssueTypeLogic>();
-            services.AddTransient<IIssueTypeEngine, IssueTypeEngine>();
-
-            services.AddTransient<IRegisterLogic, RegisterLogic>();
+            services.AddCustomTransientServices();
 
             services.Configure<AuthOptions>(Configuration.GetSection("AuthOptions"));
 
             var authOptions = Configuration.GetSection("AuthOptions").Get<AuthOptions>();
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    RequireExpirationTime = true,
-                    ValidIssuer = authOptions.Issuer,
-                    ValidAudience = authOptions.Audience,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authOptions.SecureKey))
-                };
-            });
+            services.AddCustomAuthentication(authOptions);
 
             services.AddControllers();
 
             services.AddDbContext<DataContext>(item => item.UseSqlServer(Configuration.GetConnectionString("myconn")));
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Issue Tracker", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description =
-                    "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                   {
-                       new OpenApiSecurityScheme
-                       {
-                           Reference = new OpenApiReference
-                           {
-                               Type = ReferenceType.SecurityScheme,
-                               Id = "Bearer"
-                           },
-                           Scheme = "oauth2",
-                           Name = "Bearer",
-                           In = ParameterLocation.Header,
-                       },
-                       new List<string>()
-                   }
-               });
-            });
+            services.AddSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -119,9 +66,7 @@ namespace IssueTracker
 
             app.UseHttpsRedirection();
 
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+            app.UseCustomSwagger();
 
             app.UseRouting();
 
